d837e5189a8a22375c69002b29e676cadb32cc79
Theodore Terdes <tterdes@gmail.com>
2017-08-21 23:47:50 +0800

    Move redirect to different method and add spec

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,16 +1,6 @@
 class LinksController < ApplicationController
   def show
-    @link = if params[:short_url].present?
-      Link.find_by(short_url: params[:short_url])
-    elsif params[:id].present?
-      Link.find(params[:id])
-    end
-
-    if @link
-      redirect_to @link.original_url, status: :moved_permanently
-    else
-      raise ActionController::RoutingError.new('Not Found')
-    end
+    @link = Link.find(params[:id])
   end
 
   def new
@@ -22,6 +12,14 @@ class LinksController < ApplicationController
     @new_link = Link.shrink(link_params[:original_url])
 
     render :new
+  end
+
+  def redirect
+    if @link = Link.find_by(short_url: params[:short_url])
+      redirect_to @link.original_url, status: :moved_permanently
+    else
+      raise ActionController::RoutingError.new('Not Found')
+    end
   end
 
   protected
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :links, only: [:show, :new, :create]
+  resources :links, only: [:show, :new, :create] do
+    collection do
+      get 'redirect/:short_url' => :redirect
+    end
+  end
 
-  get '/:short_url' => 'links#show'
+  get '/:short_url' => 'links#redirect'
 
   root 'links#new'
 end
